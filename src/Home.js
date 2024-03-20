3cd243d5f09223ca79f6936e5e8bae88c9d8d97e
Mohammed Nabeel <pmdnabeel11163@gmail.com>
2024-03-20 10:19:59 +0530

    first

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+export default function Home(){
+    return(
+        <>
+        <img id="homeimg" src="home.png"></img>
+        <div id="home">
+         <h3>Welcome to <span id="span-1">MCA</span>-<span id="span-2">BANK</span></h3>
+         <h4>"Online banking"</h4>
+         <p> Available 24/7 from almost any device with an internet connection</p>
+          <p>Create new Account ,balance checks and deposit your money.</p>
+
+        </div>
+        
+       
+        </>
+    )
+}
